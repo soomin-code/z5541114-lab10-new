fb3538bc835eb4f1126cbc7c5c044de5635e5aab
soomin-01-s <shinsm0115@gmail.com>
2025-08-04 19:07:23 +0900

    Update to final working Vercel URL

--- src/submission.ts
+++ src/submission.ts
@@ -1,4 +1,4 @@
 // The URL can be anything you want, as long as it contains your zID exactly once.
 // For example, 'https://z1234444-forum-deploy-test.vercel.app'
 // The URL must begin with 'https://'
-export const DEPLOYED_URL = 'https://z5541114-lab10-deploy.vercel.app';
+export const DEPLOYED_URL = 'https://z5541114-lab10-final.vercel.app';
