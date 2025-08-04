d197a29e47ad5567a60baf7aabdda6b3acccceb2
soomin-01-s <shinsm0115@gmail>
2025-08-04 15:52:51 +0900

    Fix Vercel deployment configuration

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,13 @@
-import express, { Request, Response } from 'express';
-import cors from 'cors';
-import fs from 'fs';
+import express, { Request, Response } from "express";
+import cors from "cors";
+import fs from "fs";
 
-import morgan from 'morgan';
+import morgan from "morgan";
 
-import { echo } from './echo';
-import errorHandler from 'middleware-http-errors';
-import { DATABASE_FILE, setData, addName, viewNames, clear } from './names';
-import { port, url } from './config.json';
+import { echo } from "./echo";
+import errorHandler from "middleware-http-errors";
+import { DATABASE_FILE, setData, addName, viewNames, clear } from "./names";
+import { port, url } from "./config.json";
 
 const PORT: number = parseInt(process.env.PORT || port);
 const SERVER_URL = `${url}:${PORT}`;
@@ -16,27 +16,35 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
-app.get('/', (req: Request, res: Response) => {
-  console.log('Print to terminal: someone accessed our root url!');
+app.get("/", (req: Request, res: Response) => {
+  console.log("Print to terminal: someone accessed our root url!");
   res.json({ message: "Welcome to Lab10 Deploy Server's root URL!" });
 });
 
-app.get('/echo/echo', (req: Request, res: Response) => {
+app.get("/echo/echo", (req: Request, res: Response) => {
   res.json(echo(req.query.message as string));
 });
 
-app.post('/add/name', (req: Request, res) => {
+app.post("/add/name", (req: Request, res) => {
   res.json(addName(req.body.name));
 });
 
-app.get('/view/names', (req: Request, res: Response) => {
+app.get("/view/names", (req: Request, res: Response) => {
   res.json(viewNames());
 });
 
-app.delete('/clear', (req: Request, res: Response) => {
+app.delete("/clear", (req: Request, res: Response) => {
   res.json(clear());
+});
+
+app.post("/clear", (req: Request, res: Response) => {
+  res.json(clear());
+});
+
+app.get("/echo/:message", (req: Request, res: Response) => {
+  res.json(echo(req.params.message));
 });
 
 app.use(errorHandler());
@@ -46,17 +54,20 @@ const server = app.listen(PORT, () => {
   if (fs.existsSync(DATABASE_FILE)) {
     setData(JSON.parse(String(fs.readFileSync(DATABASE_FILE))));
   } else {
-    fs.writeFileSync(DATABASE_FILE, JSON.stringify({
-      names: []
-    }));
+    fs.writeFileSync(
+      DATABASE_FILE,
+      JSON.stringify({
+        names: [],
+      })
+    );
   }
 
   console.log(`Server started at the URL: '${SERVER_URL}'`);
 });
 
-process.on('SIGINT', () => {
+process.on("SIGINT", () => {
   server.close(() => {
-    console.log('Shutting down server gracefully.');
+    console.log("Shutting down server gracefully.");
     process.exit();
   });
 });
